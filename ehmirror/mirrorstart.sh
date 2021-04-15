e1ad6c745bf322e27321e8f4c020e67b74285070
mcgjoe <mcgrathjoes@gmail.com>
2021-04-15 15:40:29 -0400

    Update mirrorstart.sh

--- ehmirror/mirrorstart.sh
+++ ehmirror/mirrorstart.sh
@@ -12,7 +12,7 @@ parse_dns () {
 
 SOURCE_DNS=$(parse_dns $SOURCE_CON_STR)
 DEST_DNS=$(parse_dns $DEST_CON_STR)
-CONSUMER_CONFIG="bootstrap.servers=$SOURCE_DNS:9093\nrequest.timeout.ms=60000\ngroup.id=example-mirrormaker-group\nexclude.internal.topics=true\nsasl.mechanism=PLAIN\nsecurity.protocol=SASL_SSL\nsasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username=\"\$ConnectionString\" password=\"$SOURCE_CON_STR\";"
+CONSUMER_CONFIG="bootstrap.servers=71.224.40.97:2181;"
 echo -e $CONSUMER_CONFIG > consumer.config
 
 PRODUCER_CONFIG="bootstrap.servers=$DEST_DNS:9093\nclient.id=mirror_maker_producer\nrequest.timeout.ms=60000\nsasl.mechanism=PLAIN\nsecurity.protocol=SASL_SSL\nsasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username=\"\$ConnectionString\" password=\"$DEST_CON_STR\";"
